829e3ffaed7ab8d7a18cad042ae59bbb80a12a51
Xenia Golbraykh <karzerin@gmail.com>
2024-11-07 20:33:42 +0300

    [+] add func formatDate , add truncateText

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,22 +1,44 @@
-import s from './Catalog.module.css'
+import s from './Catalog.module.css';
 import Card from '../Card/Card';
-function Catalog({ movies }) {
+import { format } from 'date-fns';
 
-    return (<div className={s['catalog-container']}>
-        {movies.length > 0 ? (
-            movies.map((movie) => (
-                <Card
-                    key={movie.id}
-                    image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                    title={movie.title}
-                    date={movie.release_date.split('-')[0]}
-                    description={movie.overview}
-                />
-            ))
-        ) : (
-            <p>Loading movies...</p>
-        )}
-    </div>);
+function Catalog({ movies }) {
+    const formatDate = (date) => {
+        return format(new Date(date), 'MMMM d, yyyy');
+    };
+    function truncateText(text, maxLength = 160) {
+        if (text.length <= maxLength) return text;
+        let truncated = text.slice(0, maxLength);
+
+        const lastSentenceEnd = Math.max(
+            truncated.lastIndexOf('.'),
+            truncated.lastIndexOf('!'),
+            truncated.lastIndexOf('?')
+        );
+
+        if (lastSentenceEnd !== -1) {
+            truncated = truncated.slice(0, lastSentenceEnd + 1);
+        }
+        return truncated + (truncated.length < text.length ? '..' : '');
+    }
+
+    return (
+        <div className={s['catalog-container']}>
+            {movies?.length ? (
+                movies.map((movie) => (
+                    <Card
+                        key={movie.id}
+                        image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                        title={movie.title}
+                        date={formatDate(movie.release_date)}
+                        description={truncateText(movie.overview)}
+                    />
+                ))
+            ) : (
+                <p>Loading movies...</p>
+            )}
+        </div>
+    );
 }
 
 export default Catalog;
