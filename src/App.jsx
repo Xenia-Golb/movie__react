db51d46ca4f8a2578d1d8dc41bec39dd0828caa0
Xenia Golbraykh <karzerin@gmail.com>
2024-11-07 22:13:51 +0300

    [+] add spin and alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Catalog from './components/Catalog/Catalog';
+import { Alert, Spin } from 'antd';
 
 function App() {
   const apiKey = import.meta.env.VITE_API_KEY;
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
 
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchData();
@@ -14,18 +17,42 @@ function App() {
 
   const fetchData = async () => {
     try {
+      setLoading(true);
       const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
       const data = await response.json();
       setMovies(data.results);
-      console.log(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="app">
-      <Catalog movies={movies} />
+      {loading && movies.length === 0 ? (
+        <div className="loading-container">
+          <Spin size="large" />
+        </div>
+      ) : (
+        <>
+          {error && (
+            <Alert
+              message="Error"
+              description={error}
+              type="error"
+              showIcon
+              closable
+              style={{ marginTop: 300 }}
+              onClose={() => setError(null)}
+            />
+          )}
+          {!loading && !error && <Catalog movies={movies} />}
+        </>
+      )}
     </div>
   );
 }
--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,43 +2,43 @@ import s from './Catalog.module.css';
 import Card from '../Card/Card';
 import { format } from 'date-fns';
 
+// eslint-disable-next-line react/prop-types
 function Catalog({ movies }) {
-    const formatDate = (date) => {
-        return format(new Date(date), 'MMMM d, yyyy');
-    };
-    function truncateText(text, maxLength = 160) {
-        if (text.length <= maxLength) return text;
-        let truncated = text.slice(0, maxLength);
+  const formatDate = (date) => {
+    return format(new Date(date), 'MMMM d, yyyy');
+  };
+  function truncateText(text, maxLength = 160) {
+    if (text.length <= maxLength) return text;
+    let truncated = text.slice(0, maxLength);
 
-        const lastSentenceEnd = Math.max(
-            truncated.lastIndexOf('.'),
-            truncated.lastIndexOf('!'),
-            truncated.lastIndexOf('?')
-        );
-
-        if (lastSentenceEnd !== -1) {
-            truncated = truncated.slice(0, lastSentenceEnd + 1);
-        }
-        return truncated + (truncated.length < text.length ? '..' : '');
-    }
-
-    return (
-        <div className={s['catalog-container']}>
-            {movies?.length ? (
-                movies.map((movie) => (
-                    <Card
-                        key={movie.id}
-                        image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                        title={movie.title}
-                        date={formatDate(movie.release_date)}
-                        description={truncateText(movie.overview)}
-                    />
-                ))
-            ) : (
-                <p>Loading movies...</p>
-            )}
-        </div>
+    const lastSentenceEnd = Math.max(
+      truncated.lastIndexOf('.'),
+      truncated.lastIndexOf('!'),
+      truncated.lastIndexOf('?'),
     );
+
+    if (lastSentenceEnd !== -1) {
+      truncated = truncated.slice(0, lastSentenceEnd + 1);
+    }
+    return truncated + (truncated.length < text.length ? '..' : '');
+  }
+
+  return (
+    <div className={s['catalog-container']}>
+      {
+        // eslint-disable-next-line react/prop-types
+        movies.map((movie) => (
+          <Card
+            key={movie.id}
+            image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            title={movie.title}
+            date={formatDate(movie.release_date)}
+            description={truncateText(movie.overview)}
+          />
+        ))
+      }
+    </div>
+  );
 }
 
 export default Catalog;
